bc8015759aa1eb6b736fd2ca87897a3fd794ecb7
AmitPrasad_Perficient <amit.prasad@perficient.com>
2020-06-28 00:51:56 +0530

    Adding new files and deleting few files

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.print("\nHello");
 	}
+
+	public int lengthOfString(String str) {
+		return 0;
+	}
 }
--- Str.java
+++
@@ -1,5 +0,0 @@
-public class Str {
-	public String format(String str) {
-		return "";
-	}
-}
